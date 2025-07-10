54bf968ac7fe2216e50d29f1c8abeda4ac5564ba
Douglas Wangome <douglaswangome@gmail.com>
2025-07-10 19:52:40 +0300

    Refactor server.js: Comment out dotenv config and unused MongoDB variables

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,11 +8,11 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+// let mongodb_url = 'mongodb://localhost/';
+// let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
